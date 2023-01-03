1a5a5b9f338ec915f94721e715da57b2cfb744ac
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2023-01-03 02:47:23 +0100

    refactor: how he use input

--- Test1/script.js
+++ Test1/script.js
@@ -1,12 +1,12 @@
-const result =document.querySelector('.result')
-const form = document.getElementById('form')
-let u = document.getElementById('u').value
+const result = document.querySelector('.result');
+const form = document.getElementById('form');
+let u = document.getElementById('u');
 
-
-form.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    let input1 = parseInt(u)// sir please check here 😥😥
-    console.log(input1)
-})
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // FIXED
+  let input1 = parseInt(u.value); // sir please check here 😥😥
+  console.log(input1);
+});
 
 /*** if do any calculation it usually concat instead adding 😭😭😭😭😭 */
